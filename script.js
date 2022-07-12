aae6859d5da47acfa31f4da26df0e3490e6f479a
Ricardo Borges <ricardoborges00@hotmail.com>
2022-07-11 21:21:24 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,17 @@
+let dados = ['item1', 'item2', 'item3', 'item4', 'item5']
+console.log(dados)
+
+const dadosNovos = dados.unshift('item0')
+console.log(dadosNovos)
+
+const mainItem = dados.pop()
+console.log(mainItem)
+
+const addItem = dados.push('item6', 'item7')
+console.log(addItem)
+
+const popItem = dados.shift()
+console.log(popItem)
+
+let number = [7,5,6,3,8,9,2,1,4]
+console.log(number.soft());
